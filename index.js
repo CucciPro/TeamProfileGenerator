b44df59115f25ed02cf90092ced5b1029be2bdbb
CucciPro <josej@email.arizona.edu>
2020-10-14 16:13:50 -0700

    created function to start and add manager

--- index.js
+++ index.js
@@ -3,26 +3,79 @@ const fillOutHTML = require('./src/generateMarkdown');
 const writeFile = require('./src/createFile');
 
 // array of questions for user
-const promptUser = () => {
-    return inquirer.prompt([
-        {
-          type: 'input',
-          name: 'name',
-          message: 'Please enter name:'
-        },
-        {
-          type: 'input',
-          name: 'github',
-          message: 'What is the user ID?'
-        },
-        {
-          type: 'confirm',
-          name: 'confirmAbout',
-          message: 'What is the user email?',
-          default: true
+function start() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "member",
+        message: "Which type of team member would you like to create?",
+        choices: [
+          "Manager",
+          "Engineer",
+          "Intern",
+          "I'm good!",
+        ],
+      },
+    ])
+    .then((userChoice) => {
+      switch (userChoice.member) {
+        case "Manager":
+          addManager();
+          break;
+        case "Engineer":
+          addEngineer();
+          break;
+        case "Intern":
+          addIntern();
+          break;
+        default:
+          buildTeam();
+      }
+    });
+}
+
+function addManager() {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "managerName",
+      message: "What is your manager's name?"
+    },
+    {
+      type: "input",
+      name: "managerId",
+      message: "What is your manager's id?"
+    },
+    {
+      type: "input",
+      name: "managerEmail",
+      message: "What is your manager's email?",
+      validate: (answer) => {
+        const pass = answer.match(/\S+@\S+\.\S+/);
+        if (pass) {
+          return true;
         }
-      ])
-  }
+        return "Please enter a valid email address.";
+      },
+    },
+    {
+      type: "input",
+      name: "managerOfficeNumber",
+      message: "What is your manager's office number?"
+    },
+  ])
+  .then((answers) => {
+    const manager = new Manager(
+      answers.managerName,
+      answers.managerId,
+      answers.managerEmail,
+      answers.managerOfficeNumber
+    );
+    teamMembers.push(manager);
+    start();
+  });
+}
 
 
 // function to write README file
